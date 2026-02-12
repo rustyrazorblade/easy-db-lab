d5bd214d5410d94742f3a1f001b73ea74baf66da
Jon Haddad <jon@jonhaddad.com>
2026-02-12 14:46:06 -0800

    Add ManifestTemplateService for K8s manifest placeholder substitution
    Replace manual String.replace() with Apache Commons StringSubstitutor
    using __ delimiters. The service encapsulates all variable building from
    ClusterState and applies substitution to all YAML files in a directory.
    
    Remove replacePlaceholders() from GrafanaDashboardService and the
    bucketName parameter from uploadDashboards(). Use __BUCKET_NAME__
    directly in S3 dashboard dimensions instead of Grafana variable.

--- src/main/kotlin/com/rustyrazorblade/easydblab/services/ServicesModule.kt
+++ src/main/kotlin/com/rustyrazorblade/easydblab/services/ServicesModule.kt
@@ -44,6 +44,7 @@ val servicesModule =
         factoryOf(::DefaultK3sAgentService) bind K3sAgentService::class
         factoryOf(::DefaultK8sService) bind K8sService::class
         factoryOf(::DefaultGrafanaDashboardService) bind GrafanaDashboardService::class
+        factoryOf(::DefaultManifestTemplateService) bind ManifestTemplateService::class
         factoryOf(::DefaultVictoriaBackupService) bind VictoriaBackupService::class
         factoryOf(::DefaultSidecarService) bind SidecarService::class
         factoryOf(::DefaultStressJobService) bind StressJobService::class

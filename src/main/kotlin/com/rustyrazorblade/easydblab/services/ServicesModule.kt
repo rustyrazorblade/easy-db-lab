7d933c3b11f8bba6142aedb91296eaed8086277a
Jon Haddad <jon@jonhaddad.com>
2026-02-18 22:24:00 -0800

    Migrate Pyroscope K8s configs from YAML to Fabric8 manifest builder
    Replace 5 static YAML files with a programmatic PyroscopeManifestBuilder
    that builds all Pyroscope resources (server ConfigMap, Service, Deployment,
    eBPF ConfigMap, eBPF DaemonSet) as typed Fabric8 objects. Fix the data
    directory permissions bug by using SSH chown in K8Apply instead of an
    init container.

--- src/main/kotlin/com/rustyrazorblade/easydblab/services/ServicesModule.kt
+++ src/main/kotlin/com/rustyrazorblade/easydblab/services/ServicesModule.kt
@@ -5,6 +5,7 @@ import com.rustyrazorblade.easydblab.configuration.ClusterStateManager
 import com.rustyrazorblade.easydblab.configuration.User
 import com.rustyrazorblade.easydblab.configuration.UserConfigProvider
 import com.rustyrazorblade.easydblab.configuration.grafana.GrafanaManifestBuilder
+import com.rustyrazorblade.easydblab.configuration.pyroscope.PyroscopeManifestBuilder
 import com.rustyrazorblade.easydblab.driver.CqlSessionFactory
 import com.rustyrazorblade.easydblab.driver.DefaultCqlSessionFactory
 import com.rustyrazorblade.easydblab.observability.TelemetryProvider
@@ -45,6 +46,7 @@ val servicesModule =
         factoryOf(::DefaultK3sAgentService) bind K3sAgentService::class
         factoryOf(::DefaultK8sService) bind K8sService::class
         factoryOf(::GrafanaManifestBuilder)
+        factoryOf(::PyroscopeManifestBuilder)
         factoryOf(::DefaultGrafanaDashboardService) bind GrafanaDashboardService::class
         factoryOf(::TemplateService)
         factoryOf(::DefaultVictoriaBackupService) bind VictoriaBackupService::class
